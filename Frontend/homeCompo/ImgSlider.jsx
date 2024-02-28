5be55273092e2e6bbafefa3ba55348c812e7e543
bhawnaniankit <bhawnaniankit@gmail.com>
2024-02-28 13:06:54 +0530

    Website Responsivity Improved

--- Frontend/homeCompo/ImgSlider.jsx
+++ Frontend/homeCompo/ImgSlider.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import "./ImgSlider.css"
 
 export function ImgSlider() {
+    const [slide, setSlide] = useState(1);
 
     return <>
         <div class="slider">
@@ -8,16 +10,16 @@ export function ImgSlider() {
             <span id="slide-2"></span>
             <span id="slide-3"></span>
             <div class="image-container">
-                <img src="../src/assets/mypost.jpg" class="slide" width="500" height="700" />
-                <img src="../src/assets/slider3.jpg" class="slide" width="500" height="700" />
-                <img src="../src/assets/slider1.jpg" class="slide" width="500" height="700" />
+                <img src="../src/assets/mypost.jpg" class="slide" />
+                <img src="../src/assets/slider3.jpg" class="slide" />
+                <img src="../src/assets/slider1.jpg" class="slide" />
                 <div className="test"><p><strong>Ankit Bhawnani</strong><br />
                     Developer of this application</p></div>
             </div>
             <div class="buttons">
-                <a href="#slide-1"></a>
-                <a href="#slide-2"></a>
-                <a href="#slide-3"></a>
+                <a href="#slide-1" className="btn"></a>
+                <a href="#slide-2" className="btn"></a>
+                <a href="#slide-3" className="btn"></a>
             </div>
         </div>
     </>
